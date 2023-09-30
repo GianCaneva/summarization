c1e2595a7cee7cfc2b0071168f05bf9ff4904f74
Gianfranco Caneva <gfocaneva@gmail.com>
2023-09-30 01:47:16 -0300

    Implement and adapt spacy summarization

--- summarization_mt5_base_dacsa_es.py
+++ summarization_mt5_base_dacsa_es.py
@@ -5,13 +5,8 @@ summarizer = pipeline("summarization", model="ELiRF/mt5-base-dacsa-es")
 def summarize_text(request):
 
     received_text = request.data.decode('utf-8')
-    max_text_extension = request.args.get('maxTextExtension')
-    min_text_extension = request.args.get('minTextExtension')
-    
-    print("Received Text:", received_text)
-    print("Max extension value:", max_text_extension)
-    print("Min extension value:", min_text_extension)
+    textExtension = request.args.get('textExtension')
 
-    response = summarizer(remove_bias(received_text), max_length=int(max_text_extension), min_length=int(min_text_extension), do_sample=False)[0]['summary_text']
+    response = summarizer(remove_bias(received_text), max_length=int(textExtension)+100, min_length=int(textExtension), do_sample=False)[0]['summary_text']
     print ("Model 1:" + response)
     return response
--- summarization_mt5_small_mlsum.py
+++ summarization_mt5_small_mlsum.py
@@ -5,16 +5,15 @@ summarizer = pipeline("summarization", model="LeoCordoba/mt5-small-mlsum")
 def summarize_text(request):
 
     received_text = request.data.decode('utf-8')
-    max_text_extension = request.args.get('maxTextExtension')
-    min_text_extension = request.args.get('minTextExtension')
+    textExtension = request.args.get('textExtension')
     
-    print("Received Text:", received_text)
-    print("Max extension value:", max_text_extension)
-    print("Min extension value:", min_text_extension)
+    #print("Received Text:", received_text)
+    #print("Max extension value:", max_text_extension)
+    #print("Min extension value:", min_text_extension)
 
     #print(summarizer(article, max_length=20, min_length=5))
     ##for titles: 20/5
-    response = summarizer(remove_bias(received_text), max_length=int(max_text_extension), min_length=int(min_text_extension))[0]['summary_text']
+    response = summarizer(remove_bias(received_text), max_length=int(textExtension)+100, min_length=int(textExtension))[0]['summary_text']
     print ("Model 2:" + response)
     return response
 
